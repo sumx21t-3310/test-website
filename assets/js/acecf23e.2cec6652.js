62ece4578662dc502f61e3d8247fb00fb0f0d77b
sumx21t <otoro180yen@gmail.com>
2025-07-11 09:36:17 +0900

    Deploy website - based on 486e98bf4dd15d89c1232a7d118eb82e070c89de

---
+++ assets/js/acecf23e.2cec6652.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunktest_website=self.webpackChunktest_website||[]).push([[1903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/test-website/blog","blogTitle":"Blog","authorsListPath":"/test-website/blog/authors"}')}}]);
